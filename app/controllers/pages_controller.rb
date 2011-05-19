92eba793549fd4281b861c321d43a27a33e8536b
Tony <tstaunton@taxback.com>
2011-05-19 12:17:19 +0100

    Static pages complete

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,23 +1,31 @@
 class PagesController < ApplicationController
+  
   def home
+    @title = "Home"
   end
 
   def about
+    @title = "About Us"
   end
 
   def contact
+    @title = "Contact Us"
   end
 
   def terms
+    @title = "Terms & Conditions"
   end
 
   def sitemap
+    @title = "Sitemap"
   end
 
   def privacy
+    @title = "Privacy Policy"
   end
 
   def faq
+    @title = "Frequently Asked Questions"
   end
 
 end
