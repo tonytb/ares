f7020fd65a105e19937073eaa746adc327ecca46
Tony <tstaunton@taxback.com>
2011-05-19 09:28:33 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ares::Application.config.secret_token = '63b33dccbb6f9704598d2441e0060b7ad25fd2fd5024250f3ecc96bf54d0ea46fc2bd65392801616c24aaf46abcc9646be1b43d727f24702f72dee6a5244c2a2'
