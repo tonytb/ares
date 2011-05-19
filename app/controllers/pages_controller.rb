d8d52527bbcf7c07eeb2f8f91fb27593390676aa
Tony <tstaunton@taxback.com>
2011-05-19 10:24:41 +0100

    Added a Pages controller

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,23 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def terms
+  end
+
+  def sitemap
+  end
+
+  def privacy
+  end
+
+  def faq
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Ares::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/about"
+
+  get "pages/contact"
+
+  get "pages/terms"
+
+  get "pages/sitemap"
+
+  get "pages/privacy"
+
+  get "pages/faq"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
